6787b5e466925cc0304579245727a7aff84797bf
Le Dai Thanh <thanh261220@gmail.com>
2025-08-08 23:43:14 +0700

    fix chut bug

--- app/meeting/[id]/page.tsx
+++ app/meeting/[id]/page.tsx
@@ -285,6 +285,7 @@ export default function MeetingPage() {
           ref={videoRef}
           autoPlay
           playsInline
+          muted
           className="w-full h-48 bg-gray-900 rounded-lg object-cover"
         />
         <div className="absolute bottom-2 left-2 bg-black bg-opacity-50 text-white px-2 py-1 rounded text-sm">
